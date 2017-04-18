fcb0afe9575a901cb8b01bfec40e879befb849b3
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-04-18 20:50:58 +0800

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # todolist
-mini side project
+Mini side project to play around with HTML5, CSS3 and JS.
+
+This webpage allows users to add tasks, check off tasks and to remove tasks.
--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,4 @@
-//window.onload = addHeading;
-//window.onload = fun;
-//var heading = document.createElement("div");
-//var node = document.createTextNode("Hello, World!");
-//heading.appendChild(node);
-//var body = document.getElementById("body");
-//document.body.appendChild(heading);
-function addHeading(){
-    var myHeading = document.querySelector('h1');
-    myHeading.textContent = 'Hello world!';
-}
-function fun () { 
-  // create a new div element 
-  // and give it some content 
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode("Hi there and greetings!"); 
-  newDiv.appendChild(newContent); //add the text node to the newly created div. 
 
-  // add the newly created element and its content into the DOM 
-  var currentDiv = document.getElementById("div1"); 
-  document.body.insertBefore(newDiv, currentDiv); 
-}
-//var elemDiv = document.createElement('div');
-//elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
-//document.body.appendChild(elemDiv);
-//
-//var para = document.createElement("p");
-//var node = document.createTextNode("This is new.");
-//para.appendChild(node);
-//
-//var element = document.getElementById("div");
-//var child = document.getElementById("p1");
-//element.insertBefore(para, child);
-
-// binding submit button to enter key
-function submitHandler(e){
-    if(e.keyCode==13){
-        document.getElementById("addElement").click();
-    }
-}
 
 // Create a new list item when clicking on the "Add" button
 function addElement() {
@@ -71,6 +32,13 @@ function checkedTaskHandler(ev) {
       if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
       }
+}
+
+// binding submit button to enter key
+function submitHandler(e){
+    if(e.keyCode==13){
+        document.getElementById("addElement").click();
+    }
 }
 
 window.onload = function(){
