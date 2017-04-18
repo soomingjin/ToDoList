a544f578de17096468d4b340550fdc22d1bc74d3
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-04-18 19:50:43 +0800

    webpage is now interactive, able to check tasks and remove tasks, also added stylesheet for further formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-window.onload = addHeading;
-window.onload = addElement;
-
+//window.onload = addHeading;
+//window.onload = fun;
 //var heading = document.createElement("div");
 //var node = document.createTextNode("Hello, World!");
 //heading.appendChild(node);
@@ -10,7 +9,7 @@ function addHeading(){
     var myHeading = document.querySelector('h1');
     myHeading.textContent = 'Hello world!';
 }
-function addElement () { 
+function fun () { 
   // create a new div element 
   // and give it some content 
   var newDiv = document.createElement("div"); 
@@ -32,3 +31,56 @@ function addElement () {
 //var element = document.getElementById("div");
 //var child = document.getElementById("p1");
 //element.insertBefore(para, child);
+
+// binding submit button to enter key
+function submitHandler(e){
+    if(e.keyCode==13){
+        document.getElementById("addElement").click();
+    }
+}
+
+// Create a new list item when clicking on the "Add" button
+function addElement() {
+var close = document.getElementsByClassName("close");
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("toDoList").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7"); // X symbol
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function checkedTaskHandler(ev) {
+      if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+      }
+}
+
+window.onload = function(){
+    var submit = document.getElementById("input");
+    console.log(submit);
+    submit.addEventListener("keypress", submitHandler, false);
+    
+    
+    // Add a "checked" symbol when clicking on a list item
+    var list = document.querySelector('ul');
+    list.addEventListener('click', checkedTaskHandler, false);
+    
+}
